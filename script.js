29fbb729614d134e4ce79454204ca7ee3059b779
ABHISHEK <103234864+Abhidixit1111@users.noreply.github.com>
2023-09-21 17:04:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+let audio = new Audio('songs/1.mp3');
+let btn2 = document.getElementById('play_pause');
+let pic = document.getElementById('changepic');
+let songitem = Array.from(document.getElementsByClassName('song'));
+let btn3 = document.getElementsByClassName('ss');
+let mainimg = document.getElementsByClassName('mainimg');
+let left = document.getElementById('left');
+let right = document.getElementById('right');
+let maingif=document.getElementById('maingif');
+let progressbar=document.getElementById('progressbar');
+
+audio.addEventListener('timeupdate',()=>{
+    let progress=parseInt((audio.currentTime/audio.duration)*1000);
+    progressbar.value=progress;
+})
+
+progressbar.addEventListener('change',()=>{
+    audio.currentTime=progressbar.value * audio.duration/1000;
+})
+
+btn2.addEventListener('click', () => {
+    if (audio.paused == false) {
+        audio.pause();
+        pic.src = 'cover/pause.png';
+        maingif.style.opacity=0;
+
+    }
+    else {
+        audio.play();
+        pic.src = 'cover/play-pause.png';
+        maingif.style.opacity=1;
+    }
+})
+let songs = [
+    { songName: "Barshaat aa gyi", filePath: "songs/1.mp3", coverPath: "cover/1.jpg" },
+    { songName: "Jana teri yaade", filePath: "songs/2.mp3", coverPath: "cover/2.jpg" },
+    { songName: "Dil meri n sune", filePath: "songs/3.mp3", coverPath: "cover/3.jpg" },
+    { songName: "Baithe baithe m", filePath: "songs/4.mp3", coverPath: "cover/4.jpg" },
+    { songName: "Phir bhi tumko j", filePath: "songs/5.mp3", coverPath: "cover/5.jpg" },
+    { songName: "Bachpan ka pyar.", filePath: "songs/6.mp3", coverPath: "cover/6.jpg" }
+];
+songitem.forEach((e, i) => {
+    e.getElementsByClassName('songlistimg')[0].src = songs[i].coverPath;
+    e.getElementsByClassName('songname')[0].innerHTML = songs[i].songName;
+})
+
+for (let i = 0; i < btn3.length; i++) {
+    btn3[i].addEventListener('click', () => {
+        if (audio.paused == false) {
+            pic.src = 'cover/pause.png';
+            btn3[i].src = 'cover/pause.png';
+            audio.src = `songs/${i + 1}.mp3`;
+            for (let j = 0; j < btn3.length; j++) {
+                if (j !== i) {
+                    btn3[j].src = `cover/pause.png`;
+                }
+            }
+            maingif.style.opacity=0;
+            audio.pause();
+        }
+        else {
+            audio.src = `songs/${i + 1}.mp3`;
+            pic.src = 'cover/play-pause.png';
+            btn3[i].src = 'cover/play-pause.png';
+            mainimg[0].src = `cover/${i + 1}.jpg`;
+            maingif.style.opacity=1;
+            audio.play();
+        }
+    })
+}
+
+left.addEventListener('click', () => {
+    let index = audio.src.charAt(28);
+    if (index > 1) {
+        audio.src = `songs/${--index}.mp3`;
+        audio.play();
+        pic.src = 'cover/play-pause.png';
+        btn3[index-1].src = 'cover/play-pause.png';
+        mainimg[0].src = `cover/${index}.jpg`;
+        for (let j = 0; j < btn3.length; j++) {
+            if (j !== index) {
+                btn3[j].src = `cover/pause.png`;
+            }
+        }
+        audio.play();
+    }
+})
+
+right.addEventListener('click',()=>{
+    let index=audio.src.charAt(28);
+    if(index<songs.length)
+    {
+        audio.src=`songs/${++index}.mp3`;
+        pic.src='cover/play-pause.png';
+        btn3[index-1].src='cover/play-pause.png';
+        mainimg[0].src=`cover/${index}.jpg`;
+        for (let j = 0; j < btn3.length; j++) {
+            if (j !== index) {
+                btn3[j].src = `cover/pause.png`;
+            }
+        }
+        audio.play();
+    }
+})
